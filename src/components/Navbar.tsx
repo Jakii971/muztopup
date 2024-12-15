aa5bab1f2c6507bc8ba1114fb7fb5f0068f9ddb7
Jakii971 <izzudinlagi971@gmail.com>
2024-12-15 19:50:49 +0700

    axios added

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import { Box, Text } from '@chakra-ui/react'
+
+export default function Navbar() {
+	return (
+			<Box bg="blue" w="100%" p="4">
+				<Text color="white" fontWeight='bold'>MuzTopup</Text>
+			</Box>
+	)
+}
